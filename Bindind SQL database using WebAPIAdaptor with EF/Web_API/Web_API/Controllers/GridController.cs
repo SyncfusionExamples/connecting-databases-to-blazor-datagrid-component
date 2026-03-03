b9ffe9b5cafb8dff7cb6db15a7ee681f61ccad54
Barath Aravind K <baratharavind.kannan@syncfusion.com>
2026-03-03 19:44:29 +0530

    1011622: Updated Filtering functionality

--- Bindind SQL database using WebAPIAdaptor with EF/Web_API/Web_API/Controllers/GridController.cs
+++ Bindind SQL database using WebAPIAdaptor with EF/Web_API/Web_API/Controllers/GridController.cs
@@ -2,11 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Syncfusion.Blazor;
 using Syncfusion.Blazor.Data;
 using System.Collections;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Text.Json.Serialization;
 using Web_API.Data;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Web_API.Controllers
 {
@@ -42,7 +38,8 @@ namespace Web_API.Controllers
                     var filterConditions = filterQuery.Split(new[] { " and " }, StringSplitOptions.RemoveEmptyEntries);
                     List<SearchFilter> searchFilters = new List<SearchFilter>();
                     List<WhereFilter> filters = new List<WhereFilter>();
-
+                    bool isPredicate = false;
+                    var fields = typeof(Order).GetProperties().Select(p => p.Name).ToList();
                     foreach (var condition in filterConditions)
                     {
                         // Check if the condition involves a substring search.
@@ -51,17 +48,34 @@ namespace Web_API.Controllers
                             var conditionParts = condition.Split('(', ')', '\'');
                             bool isSearch = conditionParts.Count(x => x == "substringof") > 1;
 
-                            var fields = typeof(Order).GetProperties().Select(p => p.Name).ToList();
-
                             if(isSearch)
                             {
                                 var searchValue = conditionParts[3]?.ToLower() ?? "";
+                                searchValue = searchValue == string.Empty ? conditionParts[4]?.ToLower() : searchValue;
                                 searchFilters.Add(new SearchFilter {Fields = fields, Key = searchValue, IgnoreCase = true, Operator = "contains" });
                             }
                             else
                             {
-                                var searchValue = conditionParts[2]?.ToLower() ?? "";
-                                var fieldName = conditionParts[4];
+                                int searchValueIndex, fieldNameIndex;
+                                if(conditionParts.Count() > 7)
+                                {
+                                    searchValueIndex = 3;
+                                    fieldNameIndex = 5;
+                                    isPredicate = true;
+                                }
+                                else
+                                {
+                                    searchValueIndex = 2;
+                                    fieldNameIndex = 4;
+                                }
+                                var searchValue = conditionParts[searchValueIndex]?.ToLower() ?? "";
+                                var fieldName = conditionParts[fieldNameIndex];
+
+                                if(!fields.Contains(fieldName))
+                                {
+                                    searchValue = conditionParts[searchValueIndex + 1]?.ToLower() ?? "";
+                                    fieldName = conditionParts[fieldNameIndex + 1];
+                                }
                                 
                                 filters.Add(new WhereFilter { Field = fieldName, Operator = "contains", value = searchValue, IgnoreCase = true });
                             }
@@ -79,26 +93,61 @@ namespace Web_API.Controllers
                             // Handle cases where the filter condition has fewer parts.
                             if (filterParts.Length < 6)
                             {
-                                var filterValueParts = filterParts[1].Split();
+                                var filterValueParts = filterParts[filterParts.Length-2].Split();
                                 filterField = filterValueParts[0];
+                                if (!fields.Contains(filterField))
+                                {
+                                    filterValueParts = filterParts[1].Split();
+                                    filterField = filterValueParts[0];
+                                }
                                 if (filterValueParts.Length > 1)
                                 {
-                                    filterOperator = filterValueParts[1].ToLower();
+                                    filterOperator = ConvertOperator(filterValueParts[1]);
                                 }
                                 filterValue = filterValueParts.Length > 2 ? filterValueParts[2].Trim('\'') : "";
                             }
-                            else
+                            else if(filterParts.Length >= 6)
                             {
-                                filterOperator = filterParts[0].ToLower();
-                                filterField = filterParts[2];
-                                filterValue = filterParts[4];
+                                filterField = filterParts[3];
+                                if (!fields.Contains(filterField))
+                                {
+                                    filterField = filterParts[4];
+                                    filterOperator = ConvertOperator(filterParts[2]);
+                                    filterValue = filterParts[6];
+                                }
+                                else
+                                {
+                                    filterOperator = ConvertOperator(filterParts[1]);
+                                    filterValue = filterParts[5];
+                                }
+                                isPredicate = true;
                             }
 
-                            filters.Add(new WhereFilter { Field = filterField, Operator = filterOperator, value = filterValue });
+                            filters.Add(new WhereFilter { Field = filterField, Operator = filterOperator, value = filterValue, IgnoreCase = true });
                         }
                     }
-                    dataSource = DataOperations.PerformSearching(dataSource, searchFilters);
-                    dataSource = DataOperations.PerformFiltering(dataSource, filters, filters[0].Operator);
+                    if(searchFilters.Count > 0)
+                    {
+                        dataSource = DataOperations.PerformSearching(dataSource, searchFilters);
+                    }
+                    if(filters.Count > 0)
+                    {
+                        if(isPredicate)
+                        {
+                            var mainFilter = new WhereFilter
+                            {
+                                Condition = "and",
+                                IsComplex = true,
+                                predicates = filters
+                            };
+                            List<WhereFilter> mainFilters = new List<WhereFilter>{ mainFilter};
+                            dataSource = DataOperations.PerformFiltering(dataSource, mainFilters, null);
+                        }
+                        else
+                        {
+                            dataSource = DataOperations.PerformFiltering(dataSource, filters, filters[0].Operator);
+                        }
+                    }
                 }
 
                 // Handling Sorting
@@ -199,5 +248,25 @@ namespace Web_API.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Converts OData filter operators to Syncfusion DataOperations operators
+        /// </summary>
+        private string ConvertOperator(string oDataOperator)
+        {
+            return oDataOperator?.ToLower() switch
+            {
+                "eq" => "equal",
+                "ne" => "notequal",
+                "gt" => "greaterthan",
+                "ge" => "greaterthanorequal",
+                "lt" => "lessthan",
+                "le" => "lessthanorequal",
+                "startswith" => "startswith",
+                "endswith" => "endswith",
+                "contains" => "contains",
+                _ => oDataOperator?.ToLower() ?? "equal"
+            };
+        }
     }
 }
